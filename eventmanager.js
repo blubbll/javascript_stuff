c7ce756a17d9d1005eecbf03575cd6bdd7b1febb
Blubbll <34196146+blubbll@users.noreply.github.com>
2018-02-26 10:57:06 +0100

    Create eventmanager.js

---
+++ eventmanager.js
@@ -0,0 +1,24 @@
+function addEvent(e, n, t) {
+    function r(e) {
+        var n = t.apply(this, arguments);
+        return n === !1 && (e.stopPropagation(), e.preventDefault()), n
+    }
+
+    function v() {
+        var n = t.call(e, window.event);
+        return n === !1 && (window.event.returnValue = !1, window.event.cancelBubble = !0), n
+    }
+    return e.addEventListener ? (e.addEventListener(n, r, !1), {
+        elem: e,
+        handler: r,
+        event: n
+    }) : (e.attachEvent("on" + n, v), {
+        elem: e,
+        handler: v,
+        event: n
+    })
+}
+
+function removeEvent(e) {
+    e.elem.removeEventListener ? e.elem.removeEventListener(e.event, e.handler) : e.elem.detachEvent("on" + e.event, e.handler)
+}
