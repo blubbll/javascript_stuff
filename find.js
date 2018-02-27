69f60eac975c17b2a73ee86f7df1ac8ef6f5d164
Blubbll <34196146+blubbll@users.noreply.github.com>
2018-02-27 22:01:42 +0100

    Update find.js

--- find.js
+++ find.js
@@ -1,23 +1,42 @@
-var find = {
-    byId: function(e) {
-        return q = document.getElementById(e.slice(1)), null == q ? document.documentElement : q
-    },
-    byClass: function(e) {
-        return q = document.getElementsByClassName(e.slice(1))[0], null == q ? document.documentElement : q
-    },
-    byDataId: function(e) {
-        return q = document.querySelector("[data-id='" + e + "']"), null == q ? document.documentElement : q
-    },
-    byData: function(e) {
-        return q = document.querySelector("[data-" + e + "]"), null == q ? document.documentElement : q
-    },
-    allByDataId: function(e) {
-        return q = document.querySelectorAll("[data-id='" + e + "']"), null == q ? document.documentElement : q
-    },
-    allByClass: function(e) {
-        return q = document.getElementsByClassName(e.slice(1)), null == q ? document.documentElement : q
-    },
-    byTagname: function(e) {
-        return q = document.getElementsByTagName(e)[0], null == q ? document.documentElement : q
+//Finde Objekte in HTML : )
+function find(search) {
+    
+    //attr mit val
+    if (search.indexOf('=') !== -1) {
+        type = '=';
+        attr = str.split('=')[0];
+        val = str.split('=')[1];
+        
+        //nur attr
+    } else if (search.indexOf('[') !== -1 && search.indexOf(']') !== -1) {
+        type = "[]";
+        attr = search.replace("[", '').replace("]", '');
+        
+        //normales vorgehen mit erstem Zeichen
+    } else {
+        type = search.charAt(0);
     }
-};
+
+    switch (type) {
+
+        //class
+        case '.':
+            return document.getElementsByClassName(search)[0];
+
+            //id
+        case '#':
+            return document.getElementById(search);
+
+            //attr
+        case '[]':
+            return document.querySelector('[' + attr + ']');
+
+            //attr + value
+        case '=':
+            return document.querySelector('[' + attr + '"="' + val + '"]');
+
+            //tagname
+        default:
+            return document.getElementsByTagName(search)[0];
+    }
+}
