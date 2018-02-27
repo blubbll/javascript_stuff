02aa2fabe652b06c795c5bd530996ef526e162e6
Blubbll <34196146+blubbll@users.noreply.github.com>
2018-02-27 22:19:50 +0100

    Update find.js

--- find.js
+++ find.js
@@ -1,14 +1,14 @@
-function find(search, all = false) {
+function find(search) {
     //Attr
     if (search.indexOf('=') !== -1) {
         type = '=';
         attr = str.split('=')[0];
         val = str.split('=')[1];
-     //Attr mit value
+        //Attr mit value
     } else if (search.indexOf('[') !== -1 && search.indexOf(']') !== -1) {
         type = "[]";
         attr = search.replace("[", '').replace("]", '');
-       //Normaler typ
+        //Normaler typ
     } else {
         type = search.charAt(0);
     }
@@ -16,22 +16,18 @@ function find(search, all = false) {
         //class
         case '.':
             return document.getElementsByClassName(search)[0];
-            break;
 
             //id
         case '#':
             return document.getElementById(search);
-            break;
 
             //attr
         case '[]':
             return document.querySelector('[' + attr + ']');
-            break;
 
             //attr + value
         case '=':
             return document.querySelector('[' + attr + '"="' + val + '"]');
-            break;
 
             //tagname
         default:
@@ -47,11 +43,11 @@ function findAll(search) {
         type = '=';
         attr = str.split('=')[0];
         val = str.split('=')[1];
-     //Attr mit value
+        //Attr mit value
     } else if (search.indexOf('[') !== -1 && search.indexOf(']') !== -1) {
         type = "[]";
         attr = search.replace("[", '').replace("]", '');
-       //Normaler typ
+        //Normaler typ
     } else {
         type = search.charAt(0);
     }
@@ -59,21 +55,17 @@ function findAll(search) {
         //class
         case '.':
             return document.getElementsByClassName(search);
-            break;
             //id
         case '#':
             return document.getElementsById(search);
-            break;
 
             //attr
         case '[]':
             return document.querySelectorAll('[' + attr + ']');
-            break;
 
             //attr + value
         case '=':
             return document.querySelectorAll('[' + attr + '"="' + val + '"]');
-            break;
 
             //tagname
         default:
