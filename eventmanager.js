be2bc3f760c1575fb2d8b89ba4fcbf2c58a17baa
Blubbll <34196146+blubbll@users.noreply.github.com>
2018-02-27 13:41:31 +0100

    Update eventmanager.js

--- eventmanager.js
+++ eventmanager.js
@@ -1,24 +1,17 @@
-function addEvent(e, n, t) {
-    function r(e) {
-        var n = t.apply(this, arguments);
-        return n === !1 && (e.stopPropagation(), e.preventDefault()), n
-    }
-
-    function v() {
-        var n = t.call(e, window.event);
-        return n === !1 && (window.event.returnValue = !1, window.event.cancelBubble = !0), n
-    }
-    return e.addEventListener ? (e.addEventListener(n, r, !1), {
-        elem: e,
-        handler: r,
-        event: n
-    }) : (e.attachEvent("on" + n, v), {
-        elem: e,
-        handler: v, 
-        event: n
-    })
+function addEvent(element, action, func){
+    
+    window.events_+action = func;
+    handle = window.events_+action;
+    
+    element.addEventListener(element, handle);
+    
 }
 
-function removeEvent(element, event) {
-    element.removeEventListener ? element.removeEventListener(element, event) : element.detachEvent("on" + event, element)
+function removeEvent(element, action){
+    
+    window.action_func = func;
+    handle = window.events_+action;
+    
+    element.addEventListener(element, handle);
+    
 }
