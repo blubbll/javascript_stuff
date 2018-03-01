b7c6a3c1e67c9e88957dc893b154e911c4874660
Blubbll <34196146+blubbll@users.noreply.github.com>
2018-03-01 07:32:42 +0100

    have all methods in one var (events)

--- eventmanager.js
+++ eventmanager.js
@@ -1,33 +1,28 @@
-//Add function for specific action to element
-function addEvent(element, action, func) {
-
-    window[element + action] = func;
-    
+var events = {
+  //Add function for specific action to element
+    add: function(element, action, func) {
+            window[element + action] = func;
     handle = window[element + action];
     if (document.addEventListener)
         element.addEventListener(action, handle, false);
     else if (document.detachEvent)
         element.attachEventListener(action, handle);
-
-}
-
-//Removes function for specific action of element
-function removeEvent(element, action) {
-    
-    handle = window[element + action];
-    
+    },
+  //Remove event for element
+    remove: function(element, action) {
+            handle = window[element + action];
     if (document.removeEventListener)
         element.removeEventListener(action, handle, false);
     else if (document.detachEvent)
-        element.detachEvent(action, handle);
-}
-
-//Removes all Events from element
-function removeEvents(element) {
-    element.outerHTML = element.outerHTML
-}
-
-//Remove all Events, globally.
-function removeAllEvents(){
-  document.body.innerHTML = document.body.innerHTML;
-}
+        element.detachEvent(action, handle)
+    },
+  //Removes all Events from element
+    removeAll: function(element) {
+         element.outerHTML = element.outerHTML
+    },
+  //Remove all Events, globally.
+    reset: function(e) {
+        //Remove all Events, globally.
+      document.body.innerHTML = document.body.innerHTML;
+    }
+};
