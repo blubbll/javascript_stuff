9d05c494fd37a3c90e7efafa8448637cb64b8dc5
Blubbll <34196146+blubbll@users.noreply.github.com>
2018-04-28 10:40:12 +0200

    renamed reset to clear, merged removeall

--- eventmanager.js
+++ eventmanager.js
@@ -1,28 +1,32 @@
 var events = {
-  //Add function for specific action to element
+    //Add function for specific action to element
     add: function(element, action, func) {
-            window[element + action] = func;
-    handle = window[element + action];
-    if (document.addEventListener)
-        element.addEventListener(action, handle, false);
-    else if (document.detachEvent)
-        element.attachEventListener(action, handle);
+        window[element + action] = func;
+        handle = window[element + action];
+        if (document.addEventListener)
+            element.addEventListener(action, handle, false);
+        else if (document.detachEvent)
+            element.attachEventListener(action, handle);
     },
-  //Remove event for element
+    //Remove event for element
     remove: function(element, action) {
+        //remove one
+        if (typeof action != "undefined") {
             handle = window[element + action];
-    if (document.removeEventListener)
-        element.removeEventListener(action, handle, false);
-    else if (document.detachEvent)
-        element.detachEvent(action, handle)
+            if (document.removeEventListener)
+                element.removeEventListener(action, handle, false);
+            else if (document.detachEvent)
+                element.detachEvent(action, handle)
+        }
+        //remove all
+        else if (typeof action == "undefined") {
+            element.outerHTML = element.outerHTML
+        }
     },
-  //Removes all Events from element
-    removeAll: function(element) {
-         element.outerHTML = element.outerHTML
-    },
-  //Remove all Events, globally.
-    reset: function(e) {
+
+    //Remove all Events, globally.
+    clear: function(e) {
         //Remove all Events, globally.
-      document.body.innerHTML = document.body.innerHTML;
+        document.body.innerHTML = document.body.innerHTML;
     }
 };
