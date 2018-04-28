3a5e5f89041f6f00b0f5627994dee4eb1f6c5168
Blubbll <34196146+blubbll@users.noreply.github.com>
2018-04-29 01:51:41 +0200

    Update eventmanager.js

--- eventmanager.js
+++ eventmanager.js
@@ -1,13 +1,14 @@
 var events = {
     //Add function for specific action to element
     add: function(element, action, func) {
-			if(typeof element!="undefined" && element != null)
-        window[element + action] = func;
-        handle = window[element + action];
-        if (document.addEventListener)
-            element.addEventListener(action, handle, false);
-        else if (document.detachEvent)
-            element.attachEventListener(action, handle);
+			if(element){
+					window[element + action] = func;
+					handle = window[element + action];
+					if (document.addEventListener)
+							element.addEventListener(action, handle, false);
+					else if (document.detachEvent)
+							element.attachEventListener(action, handle);
+				}
     },
     //Remove event for element
     remove: function(element, action) {
